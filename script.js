387f3b75ce2599fce77e7e5a586657d01b5048dd
sruti-sucharita <ece.22bech07@silicon.ac.in>
2024-07-14 20:44:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    const name = document.getElementById('name').value;
+    const errorMessage = document.getElementById('error-message');
+    if (!name) {
+      errorMessage.textContent = 'Please fill out the required field.';
+      return false;
+    }
+    errorMessage.textContent = ''; // Clear any previous error messages
+    alert('Form submitted successfully!');
+    return true;
+  }
